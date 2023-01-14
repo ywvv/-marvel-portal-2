815643f330263a0d25329618b43b1a43e85c65ad
ywvv <ywvvvw@gmail.com>
2023-01-14 18:31:31 +0200

    feat: add marvel-service

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./components/app/App";
+import MarvelService from "./services/MarvelService";
+
 import "./style/style.scss";
+
+const marvelService = new MarvelService();
+marvelService.getAllCharacters().then((res) => console.log(res.data.results));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
---
+++ src/services/MarvelService.js
@@ -0,0 +1,26 @@
+class MarvelService {
+  _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  _apiKey = "apikey=a352bdb01e9a5faf4c37a3fc40cd6ba6";
+
+  getResource = async (url) => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  getAllCharacters = () => {
+    return this.getResource(
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+    );
+  };
+
+  getCharacter = (id) => {
+    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+  };
+}
+
+export default MarvelService;
