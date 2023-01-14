f169014951894c0dfbe19f10cbd24d254deea502
ywvv <ywvvvw@gmail.com>
2023-01-14 21:01:06 +0200

    feat: add char-list

--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -1,51 +1,83 @@
-import "./charList.scss";
-import abyss from "../../resources/img/abyss.jpg";
+import { Component } from "react";
 
-const CharList = () => {
+import "./charList.scss";
+
+import MarvelService from "../../services/MarvelService";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/errorMessage";
+
+class CharList extends Component {
+  state = {
+    charList: [],
+    loading: true,
+    error: false,
+  };
+
+  marvelService = new MarvelService();
+
+  componentDidMount() {
+    this.marvelService
+      .getAllCharacters()
+      .then(this.onCharListLoaded)
+      .catch(this.onError);
+  }
+
+  onCharListLoaded = (charList) => {
+    this.setState({
+      charList,
+      loading: false,
+    });
+  };
+
+  onError = () => {
+    this.setState({
+      loading: false,
+      error: true,
+    });
+  };
+
+  render() {
+    const { charList, loading, error } = this.state;
+
+    return (
+      <div className="char__list">
+        {loading ? (
+          <Spinner />
+        ) : error ? (
+          <ErrorMessage />
+        ) : (
+          <View charList={charList} />
+        )}
+
+        <button className="button button__main button__long">
+          <div className="inner">load more</div>
+        </button>
+      </div>
+    );
+  }
+}
+
+const View = ({ charList }) => {
+  const noImage =
+    "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg";
+
   return (
-    <div className="char__list">
-      <ul className="char__grid">
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item char__item_selected">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-        <li className="char__item">
-          <img src={abyss} alt="abyss" />
-          <div className="char__name">Abyss</div>
-        </li>
-      </ul>
-      <button className="button button__main button__long">
-        <div className="inner">load more</div>
-      </button>
-    </div>
+    <ul className="char__grid">
+      {charList.map((char) => {
+        const isHasImg = char.thumbnail === noImage;
+
+        return (
+          <li className="char__item" key={char.id}>
+            <img
+              src={char.thumbnail}
+              alt={char.name}
+              style={isHasImg ? { objectFit: "fill" } : null}
+            />
+            <div className="char__name">{char.name}</div>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -30,6 +30,7 @@ class MarvelService {
 
   _transformCharacter = (char) => {
     return {
+      id: char.id,
       name: char.name,
       description: char.description
         ? `${char.description.slice(0, 210)}...`
