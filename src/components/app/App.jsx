2bc6e2eb2dd2d90e22b91ac7e7b78425b490280b
ywvv <ywvvvw@gmail.com>
2023-01-23 13:58:54 +0200

    feat: add comics-list

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -6,6 +6,7 @@ import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import decoration from "../../resources/img/vision.png";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import ComicsList from "../comicsList/ComicsList";
 
 const App = () => {
   const [selectedChar, setChar] = useState(null);
@@ -18,17 +19,18 @@ const App = () => {
     <div className="app">
       <AppHeader />
       <main>
-        <ErrorBoundary>
-          <RandomChar />
-        </ErrorBoundary>
-        <div className="char__content">
-          <ErrorBoundary>
-            <CharList onCharSelected={onCharSelected} />
-          </ErrorBoundary>
-          <ErrorBoundary>
-            <CharInfo charId={selectedChar} />
-          </ErrorBoundary>
-        </div>
+        <ComicsList />
+        {/*<ErrorBoundary>*/}
+        {/*  <RandomChar />*/}
+        {/*</ErrorBoundary>*/}
+        {/*<div className="char__content">*/}
+        {/*  <ErrorBoundary>*/}
+        {/*    <CharList onCharSelected={onCharSelected} />*/}
+        {/*  </ErrorBoundary>*/}
+        {/*  <ErrorBoundary>*/}
+        {/*    <CharInfo charId={selectedChar} />*/}
+        {/*  </ErrorBoundary>*/}
+        {/*</div>*/}
         <img className="bg-decoration" src={decoration} alt="vision" />
       </main>
     </div>
